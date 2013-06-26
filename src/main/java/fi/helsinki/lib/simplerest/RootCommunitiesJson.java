7ed56c996b70b74ca06cd84cfcd78c3a246de294
Anis Moubarik <anis.moubarik@helsinki.fi>
2013-06-26 08:20:46 +0300

    json fixes

--- src/main/java/fi/helsinki/lib/simplerest/RootCommunitiesJson.java
+++ src/main/java/fi/helsinki/lib/simplerest/RootCommunitiesJson.java
@@ -6,6 +6,7 @@ package fi.helsinki.lib.simplerest;
 
 import com.google.gson.Gson;
 import common.Logger;
+import java.io.Serializable;
 import org.dspace.content.Community;
 import org.dspace.core.Context;
 import org.restlet.resource.Get;
@@ -23,7 +24,7 @@ public class RootCommunitiesJson extends BaseResource{
     }
     
     @Get("json")
-        public String toJson() {
+    public String toJson() {
         Community[] communities;
         Context c = null;
         try{
@@ -33,8 +34,43 @@ public class RootCommunitiesJson extends BaseResource{
             return errorInternal(c, e.toString()).getText();
         }
         
+        /*Community class from DSpace-api won't work for Serialization to json,
+        so we use StubCommunity, and use a slow loop to create new StubCommunity array,
+        which will be Serializable and converted to json. */
         Gson gson = new Gson();
+        StubCommunity[] toJsonCommunities = new StubCommunity[communities.length];
+        for(int i = 0; i < communities.length; i++){
+            toJsonCommunities[i] = new StubCommunity(communities[i].getID(), communities[i].getName());
+        }
                 
-        return gson.toJson(communities);
+        return gson.toJson(toJsonCommunities);
     }
+}
+
+class StubCommunity implements Serializable{
+    
+    private int id;
+    private String name;
+    
+    public StubCommunity(int id, String name){
+        id = this.id;
+        name = this.name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
 }
