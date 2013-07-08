b23bb86dc79ad9bd0fa0df357d4a60abdd57dc42
Anis Moubarik <anis.moubarik@helsinki.fi>
2013-07-08 10:28:58 +0300

    json representation for Collections

---
+++ src/main/java/fi/helsinki/lib/simplerest/stubs/StubCollection.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.helsinki.lib.simplerest.stubs;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author moubarik
+ */
+public class StubCollection implements Serializable{
+    
+}
