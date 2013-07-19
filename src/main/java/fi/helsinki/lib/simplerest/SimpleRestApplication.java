9e8cf00a33240bf8b71b7173b4d1ed8717f860ef
Anis Moubarik <anis.moubarik@helsinki.fi>
2013-07-19 14:27:40 +0300

    back to text_html

--- src/main/java/fi/helsinki/lib/simplerest/SimpleRestApplication.java
+++ src/main/java/fi/helsinki/lib/simplerest/SimpleRestApplication.java
@@ -36,7 +36,7 @@ public class SimpleRestApplication extends Application {
     @Override
     public synchronized Restlet createInboundRoot() {
         Router router = new Router(getContext());
-        getMetadataService().setDefaultMediaType(MediaType.ALL);
+        getMetadataService().setDefaultMediaType(MediaType.TEXT_HTML);
         router.attach("", RootResource.class);
         router.attach("/rootcommunities", RootCommunitiesResource.class);
 
