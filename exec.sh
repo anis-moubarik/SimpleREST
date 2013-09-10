7ecb135f99a87ed5e93155979ef125d020346ca7
Anis Moubarik <anis.moubarik@helsinki.fi>
2013-09-10 12:42:27 +0300

    exec update

--- exec.sh
+++ exec.sh
@@ -1,3 +1,6 @@
-rm -rf /data/source-simplerest/
-cp -fr * /data/source-simplerest/
+if [ ! -d /data/source-simplerest/ ]; then
+   echo "creating source-simplerest folder"
+   mkdir /data/source-simplerest/
+fi
+cp -r target/simplerest.war /data/source-simplerest/
 chown -R tomcat:tomcat /data/source-simplerest/
